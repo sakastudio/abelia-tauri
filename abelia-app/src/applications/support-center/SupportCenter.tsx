c7c576cd15ff7df957dcfc7d06146af8453368d7
sakastudio <katsumi.sato@aktsk.jp>
2025-07-02 13:24:43 +0900

    1文字1文字温かみのあるコーディングしている途中

--- abelia-app/src/applications/support-center/SupportCenter.tsx
+++ abelia-app/src/applications/support-center/SupportCenter.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
-import { MantineProvider } from '@mantine/core';
+import {Flex} from '@mantine/core';
 import '@mantine/core/styles.css';
 import './SupportCenter.css';
 
-const SupportCenter: React.FC = () => {
+const SupportCenter : React.FC = () => {
 
-  return (
-    <MantineProvider>
-      <div className="support-center">
-      </div>
-    </MantineProvider>
-  );
+    return (
+        <>
+
+            <img src="assets/support-center/電話.png"
+                 style={{position: 'absolute', width: '100%', opacity: '0.5'}}></img>
+
+            <Flex direction="row" style={{height: '100%', width: '100%'}}>
+
+                <Flex flex={1} style={{backgroundColor: '#f5ed7f'}} direction="column">
+                    <Flex flex={0.16} style={{backgroundColor: 'black'}}>
+                      {/* 笑み道夫 */}
+                    </Flex>
+                    <Flex flex={0.05} style={{backgroundColor: 'white'}}>
+                      {/* 連絡先 留守番電話 */}
+                    </Flex>
+
+                    <Flex flex={0.632} style={{backgroundColor: 'black'}}>
+                      {/* 顧客一覧 */}
+                    </Flex>
+
+                    <Flex flex={0.07} style={{backgroundColor: 'white'}}>
+                      {/* 終了ボタン */}
+                    </Flex>
+                </Flex>
+
+                <Flex flex={2.1} style={{backgroundColor: '#7ff5f3'}} direction="column">
+
+
+                  <Flex flex={2.1} style={{backgroundColor: '#f6b26b'}}>
+
+
+                  </Flex>
+                  <Flex flex={2.1} style={{backgroundColor: '#6fa8dc'}}>
+
+
+                  </Flex>
+
+
+                </Flex>
+            </Flex>
+        </>
+    );
 };
 
 export default SupportCenter;
